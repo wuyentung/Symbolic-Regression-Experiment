38dcc1cb57e7776f0db71ea1f0c6ece2e76723bd
UUtung <32029557+wuyentung@users.noreply.github.com>
2021-01-24 10:50:00 +0800

    v1.0
    revise hard constraint for tree height

--- tree.py
+++ tree.py
@@ -25,7 +25,7 @@ def safe_div(a, b):
     return a / b if b else a
 def safe_pow(a, b):
     return np.abs(a) ** np.abs(b)
-operations = (
+OPERATIONS = (
     {"func": np.add, "arg_count": 2, "format_str": "({} + {})", "test": "+"},
     {"func": np.subtract, "arg_count": 2, "format_str": "({} - {})", "test": "-"},
     {"func": np.multiply, "arg_count": 2, "format_str": "({} * {})", "test": "*"},
@@ -106,31 +106,32 @@ def _generate_random_internode_count(height):
     """
     if height <= 1:
         return 1
-    max_leaf_count = 2 ** height
-    half_leaf_count = max_leaf_count // 2
-
-    # A very naive way of mimicking normal distribution
-    roll_1 = random.randint(0, half_leaf_count)
-    roll_2 = random.randint(0, max_leaf_count - half_leaf_count)
-    return roll_1 + roll_2 or half_leaf_count
+    return random.randint(1, 2**(height-1))
+    # max_leaf_count = 2 ** height
+    # half_leaf_count = max_leaf_count // 2
+    #
+    # # A very naive way of mimicking normal distribution
+    # roll_1 = random.randint(0, half_leaf_count)
+    # roll_2 = random.randint(0, max_leaf_count - half_leaf_count)
+    # return roll_1 + roll_2 or half_leaf_count
 
 
 def _generate_random_internode_values(height):
-    """Return random operations for building binary trees.
+    """Return random OPERATIONS for building binary trees.
 
     :param height: Height-1 of the binary tree.
     :type height: int
-    :return: Randomly generated operations.
+    :return: Randomly generated OPERATIONS.
     :rtype: [dict]
     """
 
     max_node_count = int(2 ** (height + 1) - 1)
     node_values = []
     for _ in range(max_node_count):
-        node_values.append(random.choice(operations))
+        node_values.append(random.choice(OPERATIONS))
     if len(node_values) == 0:
         # prevent empty list
-        node_values.append(random.choice(operations))
+        node_values.append(random.choice(OPERATIONS))
 
     return node_values
 
@@ -213,6 +214,8 @@ def _get_internal_nodes(root):
     '''
     current_level = [root]
     nodes = []
+    levels = []
+    level = 1
 
     while len(current_level) > 0:
         next_level = []
@@ -220,12 +223,14 @@ def _get_internal_nodes(root):
             if not node.is_leaf():
                 # avoid getting leaves
                 nodes.append(node)
+                levels.append(level)
             if node.left is not None:
                 next_level.append(node.left)
             if node.right is not None:
                 next_level.append(node.right)
+        level +=1
         current_level = next_level
-    return nodes
+    return nodes, levels
 
 class Node(object):
     """Represents a binary tree node.
@@ -280,9 +285,9 @@ class Node(object):
         :return:
         :rtype: Node
         '''
-        internals = _get_internal_nodes(self)
-
-        return random.choice(internals)
+        internals, levels = _get_internal_nodes(self)
+        rand_idx = random.randrange(0, len(internals))
+        return internals[rand_idx], levels[rand_idx]
 
     @property
     def program_express(self):
@@ -460,6 +465,8 @@ def tree(variables, height=4, depth=0):
     :type variables: [str]
     :param height: Height of the tree (default: 3, range: 0 - 9 inclusive).
     :type height: int
+    :param depth:
+    :type depth: int
     :return: Root node of the binary tree.
     :rtype: binarytree.Node
 
@@ -513,83 +520,90 @@ def tree(variables, height=4, depth=0):
                     _insert_random_leaf_values(getattr(root, attr))
         pass
 
-    height = int(height-depth)
+    generating_height = max(int(height - depth), 0)
 
-    # print(type(height))
-    operations: list
-    operations = _generate_random_internode_values(height - 1)
-    inter_leaf_count = _generate_random_internode_count(height - 1)
-    root = Node(operations.pop(0))
-    leaves = 0
+    if generating_height > 1:
+        # print(type(height))
+        operations: list
+        operations = _generate_random_internode_values(generating_height - 1)
+        inter_leaf_count = _generate_random_internode_count(generating_height - 1)
+        root = Node(operations.pop(0))
+        inter_leaves = 0
 
-    ## grow internal nodes, operation
-    for operation in operations:
-        node = root
-        depth = 0
-        inserted = False
-        while depth < (height-1) and not inserted:
-            if node.arg_count == 2:
-                attr = random.choice((LEFT, RIGHT))
-            else:
-                attr = LEFT
-            if getattr(node, attr) is None:
-                setattr(node, attr, Node(operation))
-                inserted = True
-            node = getattr(node, attr)
-            depth += 1
+        ## grow internal nodes, operation
+        for operation in operations:
+            node = root
+            growing_depth = 0
+            inserted = False
+            while growing_depth < (generating_height - 1) and not inserted:
+                if node.arg_count == 2:
+                    attr = random.choice((LEFT, RIGHT))
+                else:
+                    attr = LEFT
+                if getattr(node, attr) is None:
+                    setattr(node, attr, Node(operation))
+                    inserted = True
+                node = getattr(node, attr)
+                growing_depth += 1
 
-        if inserted and depth == (height-1):
-            leaves += 1
-        if leaves == inter_leaf_count:
-            break
-
-
-    ## grow leaves
-    _insert_random_leaf_values(root)
+            if inserted and growing_depth == (generating_height - 1):
+                inter_leaves += 1
+            if inter_leaves == inter_leaf_count:
+                break
+        ## grow leaves
+        _insert_random_leaf_values(root)
+    else:
+        root = Node(value= random.choice(random.choice((numbers, variables))), leaf=True)
 
     return root
 #%%
-## select_random_node
-def select_random_node(selected, parent, depth):
-    '''
-
-    :param selected:
-    :type selected: Node
-    :param parent:
-    :type parent: Node
-    :param depth:
-    :type depth: int
-    :return:
-    :rtype: Node
-    '''
-    current_level = [selected]
-    result = []
-
-    while len(current_level) > 0:
-        next_level = []
-        for node in current_level:
-            result.append(node)
-            if node.left is not None:
-                next_level.append(node.left)
-            if node.right is not None:
-                next_level.append(node.right)
-        current_level = next_level
-    choose = result[random.randrange(0, selected.size)]
-    print("choose")
-    choose.program_print()
-    return choose
-    # if 2 == selected.height:
-    #     return parent
-    # # favor nodes near the root
-    # if random.randint(0, 10) < 2*depth:
-    #     return selected
-    # if 1 == selected.arg_count:
-    #     return select_random_node(selected.left, selected, depth+1)
-    # return select_random_node(random.choice((selected.left, selected.right)), selected, depth+1)
+# ## select_random_node
+# def select_random_node(selected, parent, depth):
+#     '''
+#
+#     :param selected:
+#     :type selected: Node
+#     :param parent:
+#     :type parent: Node
+#     :param depth:
+#     :type depth: int
+#     :return:
+#     :rtype: Node
+#     '''
+#     current_level = [selected]
+#     result = []
+#     result_level = []
+#     level = 1
+#     while len(current_level) > 0:
+#         next_level = []
+#         for node in current_level:
+#             result.append(node)
+#             result_level.append(level)
+#             if node.left is not None:
+#                 next_level.append(node.left)
+#             if node.right is not None:
+#                 next_level.append(node.right)
+#             level +=1
+#         current_level = next_level
+#     chosen_ind = random.randrange(0, selected.size)
+#     chosen_node = result[chosen_ind]
+#     chosen_level = result_level[chosen_ind]
+#
+#     # print("choose")
+#     # choose.program_print()
+#     return chosen_node, chosen_level
+#     # if 2 == selected.height:
+#     #     return parent
+#     # # favor nodes near the root
+#     # if random.randint(0, 10) < 2*depth:
+#     #     return selected
+#     # if 1 == selected.arg_count:
+#     #     return select_random_node(selected.left, selected, depth+1)
+#     # return select_random_node(random.choice((selected.left, selected.right)), selected, depth+1)
 
 #%%
 ## mutation
-def do_mutate(root):
+def do_mutate(root, col_name):
     '''
 
     :param root:
@@ -598,21 +612,11 @@ def do_mutate(root):
     :rtype: Node
     '''
     offspring = deepcopy(root)
-    mutate_parent = offspring.rand_internal()
+    mutate_parent, parent_level = offspring.rand_internal()
 
-    setattr(mutate_parent, _randChildAttr(mutate_parent), tree(col_name, depth=mutate_parent.height -1))
+    setattr(mutate_parent, _randChildAttr(mutate_parent), tree(col_name, depth=parent_level + 1))
     return offspring
 
-#%%
-col_name = ['a', 'b', 'c']
-df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [0.7, 0.8, 0.9], [0.2, 0.3, 1.1]]),
-                   columns=col_name)
-tt = tree(col_name, height=3)
-tt.program_print()
-#%%
-for _ in range(100):
-    t2 = do_mutate(tt)
-    t2.program_print()
 
 
 #%%
@@ -633,8 +637,8 @@ def do_xover(selected1, selected2):
     # print(xover_parent1.program_print())
     # print("\np2\n")
     # print(selected2.program_print())
-    xover_parent1 = offspring.rand_internal()
-    xover_parent2 = selected2.rand_internal()
+    xover_parent1, p1_level = offspring.rand_internal()
+    xover_parent2, p2_level = selected2.rand_internal()
 
     setattr(xover_parent1, _randChildAttr(xover_parent1), xover_parent2.rand_child())
     # if 2 == xover_parent1.arg_count:
@@ -644,22 +648,7 @@ def do_xover(selected1, selected2):
     # setattr(xover_parent1, attr1, xover_parent2.rand_child())
     return offspring
 #%%
-col_name = ['a', 'b', 'c']
-df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [0.7, 0.8, 0.9], [0.2, 0.3, 1.1]]),
-                   columns=col_name)
-tt = tree(col_name, height=3)
-t1 = tree(col_name, height=3)
-#%%
-tt.program_print()
-t1.program_print()
-
-#%%
-for _ in range(10):
-    t2 = do_xover(tt, t1)
-    t2.program_print()
-## get_random_parent
-#%%
-def get_random_root(population, fitness, TOURNAMENT_SIZE):
+def get_random_root(population, fitness, TOURNAMENT_SIZE, POP_SIZE):
     # randomly select population members for the tournament
     tournament_members = [
         random.randint(0, POP_SIZE - 1) for _ in range(TOURNAMENT_SIZE)]
@@ -667,33 +656,34 @@ def get_random_root(population, fitness, TOURNAMENT_SIZE):
     member_fitness = [(fitness[i], population[i]) for i in tournament_members]
     # print("pick: ")
     # min(member_fitness, key=lambda x: x[0])[1].program_print()
+
     return min(member_fitness, key=lambda x: x[0])[1]
 
 ## get_offspring
 ## TODO: XOVER_PCT into fun.
-def get_offspring(population, fitness, TOURNAMENT_SIZE):
-    parent1 = get_random_root(population, fitness, TOURNAMENT_SIZE)
+def get_offspring(population, fitness, TOURNAMENT_SIZE=3, XOVER_PCT, POP_SIZE, col_name):
+    parent1 = get_random_root(population, fitness, TOURNAMENT_SIZE, POP_SIZE)
     if random.random() < XOVER_PCT:
-        parent2 = get_random_root(population, fitness, TOURNAMENT_SIZE)
+        parent2 = get_random_root(population, fitness, TOURNAMENT_SIZE, POP_SIZE)
         # print("do xover")
         return do_xover(parent1, parent2)
     else:
         # print("do mutate")
-        return do_mutate(parent1)
+        return do_mutate(parent1, col_name)
 
 def compute_fitness(root, prediction, REG_STRENGTH, y_true):
     '''
 
-    :param root:
-    :type root: Node
-    :param prediction:
-    :type prediction: list
-    :param REG_STRENGTH:
-    :param y_true:
-    :type y_true: dataframe
-    :return:
-    :rtype: float
-    '''
+        :param root:
+        :type root: Node
+        :param prediction:
+        :type prediction: list
+        :param REG_STRENGTH:
+        :param y_true:
+        :type y_true: dataframe
+        :return:
+        :rtype: float
+        '''
     # print(np.subtract(prediction, y_true.to_list()))
     # print(np.subtract(prediction, y_true.to_list()) ** 2)
     mse = np.average(np.subtract(prediction, y_true.to_list()) ** 2)
@@ -702,219 +692,52 @@ def compute_fitness(root, prediction, REG_STRENGTH, y_true):
     #    penalty = 101
     if "x" not in root.program_express and "y" not in root.program_express:
         penalty = 10
-    return mse, mse *root.size *penalty
-#%%
-col_name = ['a', 'b', 'c']
-df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [0.7, 0.8, 0.9], [0.2, 0.3, 1.1]]),
-                   columns=col_name)
-tt = tree(col_name, height=3)
-
-#%%
-temp2 = []
-temp2=evaluate(tt, df)
-
-print(df)
-tt.program_print()
-print(temp2)
-#%%
-print(temp2[1])
-
-# %%
-
-## data generate
-
-np.random.seed(0)
-
-n = 10
-input_range_low = 100
-input_range_up = 200
-output_range_low = 20
-output_range_up = 500
-
-
-def generate_uniform_data(lower, upper, n, col=1):
-    return np.random.uniform(lower, upper, n * col).reshape(n, col)
-
-
-x = generate_uniform_data(input_range_low, input_range_up, n, 2)
-y = generate_uniform_data(output_range_low, output_range_up, n, 2)
-
-## 製作 slope
-slope = (y.T[1] / y.T[0])
-
-## 參數設定
-coe_x1x2 = 1.1
-pow_x1 = 0.3
-pow_x2 = 0.4
-coe_y1_eff = (-1)
-
-y1_eff = coe_x1x2 * (x.T[0] ** pow_x1) * (x.T[1] ** pow_x2) / (slope + 1)
-y2_eff = coe_x1x2 * (x.T[0] ** pow_x1) * (x.T[1] ** pow_x2) + coe_y1_eff * y1_eff
-y2_eff =  2 * (x.T[0] * x.T[1]  )# -  y1_eff
-# y2_eff = x.T[0] ** 0.3#* x.T[1]  )# -  y1_eff
-
-y1_eff = y1_eff.reshape(n, 1)
-
-## round
-y2_eff = np.round(y2_eff, 2)
-y1_eff = np.round(y1_eff, 2)
-
-x = np.round(x, 2)
-
-
-# %%
-
-## data for SR
-d = {'y2': y2_eff.T,
-     'x_1': x.T[0],
-     'x_2': x.T[1],
-     'y_1': y1_eff.T[0],
-     }
-
-data = pd.DataFrame(data=d)
-print(data.head())
-y_true = data.pop("y2")
-print(data.head())
-print(data.columns.to_list())
-# %%
-
-### main
-# seed(SEED)
-POP_SIZE = 500
-# NEW_POP_PCT = 0.1
-col_name = data.columns.to_list()
-population = [tree(col_name) for _ in range(POP_SIZE)]
-#%%
-## TODO: revise old program
-TOURNAMENT_SIZE = 3
-XOVER_PCT = 0.7
-REG_STRENGTH = 2
-MAX_GENERATIONS = 2000
-select_depth = 10
-
-pen_global_best = global_best = float("inf")
-unchange_score = 0
-ts = time.time()
-for gen in range(MAX_GENERATIONS):
-    t1 = time.time()
-    fitness = []
-    change_flag = 1
-    kk=0
-    for prog in population:
-        # print(kk)
-        kk += 1
-        # prog.program_print()
-        prediction = [
-            evaluate(prog, data)]
-        # print(type(prediction))
-        score, pen_score = compute_fitness(prog, prediction, REG_STRENGTH, y_true)
-        # print(score)
-        if np.isnan(score):
-            score = pen_score = np.inf
-        fitness.append(score)
-        if np.isinf(score):
-            continue
-
-        if pen_score < pen_global_best:
-            pen_global_best = pen_score
-            global_best = score
-            best_pred = prediction
-            best_prog = prog
-            change_flag = 0
-
-    if change_flag:
-        unchange_score = unchange_score + 1
     else:
-        unchange_score = 0
-    # print(unchange_score)
+        penalty = 1
+    return mse
+#%%
+if __name__ == '__main__':
 
-    prog_express = best_prog.program_express
-    t2 = time.time()
-    print(
-        "\nunchange_score: %d\nGeneration: %d\nBest Score: %.2f\nPen Best Score: %.2f\nMedian score: %.2f\nBest program: %s\nTime used: %d sec\n"
-        % (
-            unchange_score,
-            gen,
-            global_best,
-            pen_global_best,
-            pd.Series(fitness).median(),
-            prog_express,
-            t2 - t1,
-        )
-    )
+    col_name = ['a', 'b', 'c']
+    df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [0.7, 0.8, 0.9], [0.2, 0.3, 1.1]]),
+                       columns=col_name)
+    tt = tree(col_name, height=3)
+    tt.program_print()
+    #%%
+    for _ in range(100):
+        t2 = do_mutate(tt)
+        t2.program_print()
+    #%%
+    col_name = ['a', 'b', 'c']
+    df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [0.7, 0.8, 0.9], [0.2, 0.3, 1.1]]),
+                      columns=col_name)
+    tt = tree(col_name, height=3)
+    t1 = tree(col_name, height=3)
+    #%%
+    tt.program_print()
+    t1.program_print()
 
-    best_count = best_prog.size
+    #%%
+    for _ in range(10):
+        t2 = do_xover(tt, t1)
+        t2.program_print()
+    ## get_random_parent
+    #%%
+    col_name = ['a', 'b', 'c']
+    df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [0.7, 0.8, 0.9], [0.2, 0.3, 1.1]]),
+                       columns=col_name)
+    tt = tree(col_name, height=3)
 
-    ## break criteria
-    if unchange_score == 200:
-        break
-    if pen_global_best < 0.05 and best_count < 2 ** 5 - 1:
-        break
-    # if pen_global_best < 0.01:
-    #     break
+    #%%
+    temp2 = []
+    temp2=evaluate(tt, df)
 
-    """
-    ## parameter control
-    if select_depth > 4 and best_count > 2**4 -1:
-        select_depth = select_depth -2
-        REG_STRENGTH = REG_STRENGTH +2
-        print("updated select_depth: ", select_depth)
-        print("updated REG_STRENGTH: ", REG_STRENGTH, "\n")
+    print(df)
+    tt.program_print()
+    print(temp2)
+    #%%
+    print(temp2[1])
 
 
 
-    if pen_global_best > 100 and select_depth < 20:
-        select_depth = select_depth +2
-        REG_STRENGTH = round(REG_STRENGTH -0.3, 1)
-        print("updated select_depth: ", select_depth, "\n")
-        print("updated REG_STRENGTH: ", REG_STRENGTH, "\n")
-    """
 
-    ## make it easier to concate
-    if unchange_score == 100:
-        # select_depth = 10
-        # REG_STRENGTH = 2
-        TOURNAMENT_SIZE = 10
-        NEW_POP_PCT = 0
-        print("set select_depth: %d, REG_STRENGTH: %d, TOURNAMENT_SIZE: %d\n"
-              % (
-                  select_depth,
-                  REG_STRENGTH,
-                  TOURNAMENT_SIZE,
-              ))
-
-    # if "x" not in prog_express and "y" not in prog_express or best_count > 2 ** 5 - 1:
-    #     if best_prog in population:
-    #         print("REMOVE best_prog", "\n")
-    #         pen_global_best = float("inf")
-    #         population.remove(best_prog)
-    #         population.append(tree(col_name))
-    #     NEW_POP_PCT = 0.2
-    #     population = [
-    #         get_offspring(population, fitness, TOURNAMENT_SIZE)
-    #         for _ in range(round(POP_SIZE * (1.0 - NEW_POP_PCT)))]
-    # else:
-    NEW_POP_PCT = 0.05
-    if unchange_score > 99:
-        NEW_POP_PCT = 0
-    population = [
-        get_offspring(population, fitness, TOURNAMENT_SIZE)
-        for _ in range(round(POP_SIZE * (1.0 - NEW_POP_PCT)) - 1)]
-    population.append(best_prog)
-    print("new gen")
-
-    new_pop = [tree(col_name) for _ in range(round(POP_SIZE * NEW_POP_PCT))]
-    population = population + new_pop
-
-tf = time.time()
-print("Best score: %f" % pen_global_best)
-print("Best program: %s" % prog_express)
-print("Total time: %d sec" % (tf - ts))
-
-# 修改為你要傳送的訊息內容
-m = "\n" + "Total time: %d sec" % (tf - ts)
-message = str(prog_express) + "\n" + m
-# 修改為你的權杖內容
-token = 'CCgjmKSEGamkEj9JvhuIkFNYTrpPKHyCb1zdsYRjo86'
-
-lineNotifyMessage(token, message)
